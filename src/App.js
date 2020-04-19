63adb74fac12298d41d163dc8f5c687c8386a477
Vitor Vianna <vitorviannaribeiro@gmail.com>
2020-04-19 19:56:19 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div id="app">Opa</div>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.js';
+import '@babel/polyfill';
+
+ReactDOM.render(<App />, document.getElementById('root'));
